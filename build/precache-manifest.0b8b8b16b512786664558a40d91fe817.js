f25230c07ec0fa6b064fb484928154fee675b643
muhammadumair185 <mumairhamid30@gmail.com>
2024-10-21 10:04:31 +0500

    script changes

---
+++ build/precache-manifest.0b8b8b16b512786664558a40d91fe817.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42a2584874f205bf9276",
+    "url": "/static/css/main.b71734cf.chunk.css"
+  },
+  {
+    "revision": "42a2584874f205bf9276",
+    "url": "/static/js/main.42a25848.chunk.js"
+  },
+  {
+    "revision": "e79c0ad755a5636be7f2",
+    "url": "/static/js/1.e79c0ad7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2d0c79c4622957363884f3c2fd23bb90",
+    "url": "/index.html"
+  }
+];
